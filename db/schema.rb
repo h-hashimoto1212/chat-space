a8297e5de721b19b3fda0c54b0b6eb9887ab640b
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-09 20:57:41 +0900

    group routed and migrated
    table created
    ## table: groups
    
    |Column|Type|Options|
    |------|----|-------|
    |group_name|string|null: false, unique: true|

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191209075549) do
+ActiveRecord::Schema.define(version: 20191209112323) do
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "group_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
