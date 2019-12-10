7912051c150a3bc1ff52cd564e3a02b29ea40ff4
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-10 20:06:40 +0900

    route for messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "group#index"
   resources :user, only: [:edit, :update]
   resources :group, only: [:new, :create, :edit, :update]
+    resources :messages, only: [:index]
   post "/group/new", to: "group#create"
 end
