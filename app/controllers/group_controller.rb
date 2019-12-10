b977ebe424e705ebab0da450a75c3ce92d18b82b
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-10 20:06:21 +0900

    redirects for invalid updates
    + some refactoring
    def setgroup

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,4 +1,5 @@
 class GroupController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
     @group = Group.all
@@ -12,30 +13,31 @@ class GroupController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました。'
+      redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new  
     end
   end
 
-  def show
-  
-  end
-
   def update
-  
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを変更しました'
+    else
+      render :edit
+    end
   end
 
   def edit
-    
+
   end
 
-  def destroy
-  
-  end
-
+  private
   def group_params
     params.require(:group).permit(:group_name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
   
 end
