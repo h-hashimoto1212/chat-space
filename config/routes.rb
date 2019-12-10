50109fed2b28876c56c9de1e1291c31c14cc4d3b
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-10 16:02:20 +0900

    message#index to group#index
    changed root to group#index
    moved index.html.haml and templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "group#index"
   resources :user, only: [:edit, :update]
   resources :group, only: [:new, :create, :edit, :update]
   post "/group/new", to: "group#create"
