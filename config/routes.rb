4bbfca50b5955458de34c6bef9359e230ae3a8ef
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-10 15:56:29 +0900

    new group create function implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :user, only: [:edit, :update]
-  resources :group, only: [:new, :edit, :update]
+  resources :group, only: [:new, :create, :edit, :update]
+  post "/group/new", to: "group#create"
 end
