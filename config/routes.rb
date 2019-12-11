a4895cb8a092cf2e0b54d7400a5a080a09c21a17
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-11 18:47:17 +0900

    messages controller routed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "group#index"
   resources :user, only: [:edit, :update]
-  resources :group, only: [:new, :create, :edit, :update]
+  resources :group, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+  end
   post "/group/new", to: "group#create"
 end
