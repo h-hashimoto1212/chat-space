05a64d3f57869820890b58d19d94337b30a4e5bc
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-17 15:35:11 +0900

    API set
    users#index defined and routed
    users#index view created as jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "group#index"
-  resources :user, only: [:edit, :update]
+  resources :user, only: [:index, :edit, :update]
   resources :group, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
