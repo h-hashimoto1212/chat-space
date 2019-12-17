85b421063be7517393901c9f9850a6dd9819a02e
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-17 15:17:22 +0900

    indents added to message.js
    for ease for your eyes

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,32 +2,32 @@ $(function(){
   function buildMessage(message){
     if (message.image.url) {
       var html = `<div class="message">
-                  <div class="message--top">
-                  <div class="message--top__user">
-                  ${message.name}
-                  </div>
-                  <div class="message--top__time_stamp">
-                  ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="message__text">
-                  ${message.text}
-                  </div>
-                  <img class="message__image" src="${message.image.url}" alt="">
+                    <div class="message--top">
+                      <div class="message--top__user">
+                        ${message.name}
+                      </div>
+                      <div class="message--top__time_stamp">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__text">
+                        ${message.text}
+                    </div>
+                    <img class="message__image" src="${message.image.url}" alt="">
                   </div>`
     } else {
       var html = `<div class="message">
-                  <div class="message--top">
-                  <div class="message--top__user">
-                  ${message.name}
-                  </div>
-                  <div class="message--top__time_stamp">
-                  ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="message__text">
-                  ${message.text}
-                  </div>
+                    <div class="message--top">
+                      <div class="message--top__user">
+                        ${message.name}
+                      </div>
+                      <div class="message--top__time_stamp">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__text">
+                      ${message.text}
+                    </div>
                   </div>`
     }
     return html;
@@ -47,7 +47,6 @@ $(function(){
     .done(function(data){
       var html = buildMessage(data);
       $('.messages').append(html);
-      
       $('.messages').animate({ scrollTop : $('.messages')[0].scrollHeight});
       $('#new_message').get(0).reset();
       $('.send').prop('disabled', false);
