da1834754542797593e51e35b2d944478b749e24
h-hashimoto1212 <58471583+h-hashimoto1212@users.noreply.github.com>
2019-12-23 19:09:57 +0900

    capitalized

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,8 +36,8 @@ end
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-  aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
 # secrets.yml用のシンボリックリンクを追加
